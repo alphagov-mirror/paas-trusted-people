99a6aee196989cb8acd84086e10ab00ad6b1f276
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-04 18:14:28 +0000

    Add spec test for auditors to not be dangerous
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -9,4 +9,21 @@ RSpec.describe 'users.yml' do
       expect { YAML.load(users_contents) }.not_to raise_error
     end
   end
+
+  context 'users and roles' do
+    let(:team_roles) { YAML.load(users_contents).dig('meta', 'team_roles') }
+    let(:users) { YAML.load(users_contents)['users'] }
+
+    it 'should not give auditors any dangerous permissions in any accounts' do
+      auditor_access_role = team_roles.dig('auditor_access')
+
+      roles = auditor_access_role.values.flatten.map { |r| r['role'] }.uniq
+
+      expect(roles).not_to include('bosh-admin')
+      expect(roles).not_to include('cf-admin')
+      expect(roles).not_to include('grafana-admin')
+
+      expect(roles.grep(/admin/)).to be_empty
+    end
+  end
 end
